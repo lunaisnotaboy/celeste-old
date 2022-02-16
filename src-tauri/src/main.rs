f52e88d555e60e937952d24ffa868642bf0353db
Erin Nova <contact@the-system.eu.org>
2022-02-16 13:14:04 -0500

    Add basic systray functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,30 +3,66 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::Manager;
+use tauri::{Manager, SystemTray, SystemTrayMenu, CustomMenuItem, SystemTrayMenuItem, SystemTrayEvent};
+
 mod setup;
 mod structures;
 
 fn main() {
-  tauri::Builder::default()
-      .setup(|app| {
-          let splashscreen_window = app.get_window("splashscreen").unwrap();
-          let main_window = app.get_window("main").unwrap();
-          // Perfom initialization code on new task so the app doesn't freeze
-          tauri::async_runtime::spawn(async move {
+    // System tray setup
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
+    let system_tray = SystemTray::new()
+        .with_menu(tray_menu);
+
+    tauri::Builder::default()
+        .system_tray(system_tray)
+        // Deal with system tray events
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("left click!");
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+                match id.as_str() {
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    "hide" => {
+                        let window = app.get_window("main").unwrap();
+                        window.hide().unwrap();
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        })
+        .setup(|app| {
+            let splashscreen_window = app.get_window("splashscreen").unwrap();
+            let main_window = app.get_window("main").unwrap();
+            // Perfom initialization code on new task so the app doesn't freeze
+            tauri::async_runtime::spawn(async move {
                 setup::setup();
 
                 // After it's done, close the splashscreen and display main window
                 splashscreen_window.close().unwrap();
                 main_window.show().unwrap();
             });
-          Ok(())
+            Ok(())
         })
-      .manage(structures::Database {})
-      .invoke_handler(tauri::generate_handler![
-                      setup::cmd_test,
-                    ])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+        .manage(structures::Database {})
+        .invoke_handler(tauri::generate_handler![
+            setup::cmd_test,
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
